6996696be6c37c3fd539110979920d033e576b03
Ali Rezaei <ali.rezaei09@gmail.com>
2022-01-22 14:51:37 +0330

    Default Gradle JDK to 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,11 +36,11 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
     // Each feature module that is included in settings.gradle.kts is added here as dynamic feature
